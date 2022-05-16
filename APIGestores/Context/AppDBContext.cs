d506769db32cfb9ea4f0f82bebf08542699d125b
JefVivero <jefersonalonsov@gmail.com>
2022-05-16 10:24:59 -0500

    AddDbContext and connetion DataBase

---
+++ APIGestores/Context/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using APIGestores.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGestores.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Gestores_bd> gestores_bd;
+    }
+}
